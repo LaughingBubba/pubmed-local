3950378e8d1a14a8cc15424daee1f5b840935cc3
LaughingBubba <laughingbubba@outlook.com>
2022-01-14 05:10:57 +0000

    add no load option

--- run.js
+++ run.js
@@ -6,13 +6,21 @@ const load = require('./src/load.js')
 main()
 
 async function main() {
+	
+	var no_load = false
+	var args = process.argv.slice(2)
+	
+	if (args.length >= 2 && args[1].toUpperCase() == 'NOLOAD') {
+		no_load = true
+	} 
+	
 	try {
 		await init_dbs.run()
 		do {
 			await ftp_files.run()
 			var md5_fails = await check_md5s.run()
 		} while (md5_fails > 0)
-		await load.run()		
+		if (!no_load) await load.run()		
 	} catch (err) {
 		console.log(err)	
 	}
