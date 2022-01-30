7d75dbc75b154b7e2296c7de31ca044764119d43
LaughingBubba <laughingbubba@outlook.com>
2022-01-30 00:03:41 +0000

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use dotenv::dotenv;
 use rusqlite::{Connection, Result};
-use std::env;
+use mongodb::{bson::doc, options::ClientOptions, Client};
+use std::env; 
 
 pub fn init_dbs() {
 	println!("initialising DBs");
@@ -9,6 +10,10 @@ pub fn init_dbs() {
 	match init_sqlite() {
 		Ok(v) => println!("{:?} items in the DB", v),
 		Err(e) => panic!("Could not initialise sqlite ({})", e)
+	}
+	match init_mongo() {
+		Ok(v) => println!("{:?} mongo initialised", v),
+		Err(e) => panic!("Could not initialise monogo ({})", e)
 	}
 	
 // 	let mongo_uri: String;	
@@ -67,6 +72,28 @@ pub fn init_dbs() {
 		}
 		
 		println!("{} items", files);
+		Ok(())
+	}
+	
+	async fn init_mongo() -> mongodb::error::Result<()> {
+		// Parse your connection string into an options struct
+		let mut client_options =
+			ClientOptions::parse("mongodb://root:access@localhost:27017")
+				.await?;
+		// Manually set an option
+		client_options.app_name = Some("pubmed_db".to_string());
+		// Get a handle to the cluster
+		let client = Client::with_options(client_options)?;
+		// Ping the server to see if you can connect to the cluster
+		client
+			.database("admin")
+			.run_command(doc! {"ping": 1}, None)
+			.await?;
+		println!("Connected successfully.");
+		// List the names of the databases in that cluster
+		for db_name in client.list_database_names(None, None).await? {
+			println!("{}", db_name);
+		}
 		Ok(())
 	}
 }
