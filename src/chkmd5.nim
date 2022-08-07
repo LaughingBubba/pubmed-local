2dfe98002cb0075a403a13f371e5e484bbf10329
LaughingBubba <joseph.szili@gmail.com>
2022-08-07 15:06:47 +1000

    make a start on checking the files

--- src/chkmd5.nim
+++ src/chkmd5.nim
@@ -1,8 +1,23 @@
 import dotenv, os, strutils
+import zip/gzipfiles
+import streams
+import md5
 
 load()
 const file_name = "pubmed22n1122.xml.gz"
 let source = getEnv("pml_downloads") & file_name
+echo "Source Md5:   ", $toMD5(readFile(source))
+
+echo "Expected MD5: ", readFile(source & ".md5").split("=")[1].strip()
+
 let target = getEnv("pml_xml") & file_name.split(".gz")[0]
+
+let gz = newGzFileStream(source)
+var x: File = open(target, fmWrite)
+
+while not gz.atEnd():
+  x.write(gz.readLine())
+x.close
+
 echo source
 echo target
